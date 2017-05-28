16d11fbc5ebd7d0ec101570248873246c22af9d4
iMacFernando <iMacFernando@192.168.1.5>
2017-05-29 00:23:25 +0200

    Revisión 1 del archivo main.java. Se ha añadido una nueva instancia del objeto Empleado y algunos comentarios.

--- src/ed06_tareaproyecto/Main.java
+++ src/ed06_tareaproyecto/Main.java
@@ -15,19 +15,30 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // creación de una instancia de Empleado
         Empleado empleado1 = new Empleado("1245678M","Perico de los Palotes");
         
+        //inicialización de atributos de la instancia
         empleado1.setDireccion("C/del Pez,6");
         empleado1.setNumHijos(3);
+
+        
+        //Creamos un nuevo empleado
+        Empleado empleado2 = new Empleado ("9873256H","Mariano de Triana");
+        //inicialización de atributos de la instancia
+        empleado2.setDireccion("Pza. de la Aurora, 2");
+        empleado2.setNumHijos(1);
+        
         imprimir_emp(empleado1);
+        imprimir_emp(empleado2);
     }
 
-    public static void imprimir_emp(Empleado empleado1) {
-        System.out.println("DNI: "+empleado1.getDni());
-        System.out.println("Nombre: "+empleado1.getNombre());
-        System.out.println("Cargo: "+empleado1.getCargo());
-        System.out.println("Número de hijos: "+empleado1.getNumHijos());
-        System.out.println("Sueldo: "+empleado1.calcula_sueldo(1200));
+    public static void imprimir_emp(Empleado queEmpleado) {
+        System.out.println("DNI: "+ queEmpleado.getDni());
+        System.out.println("Nombre: "+ queEmpleado.getNombre());
+        System.out.println("Cargo: "+ queEmpleado.getCargo());
+        System.out.println("Número de hijos: "+ queEmpleado.getNumHijos());
+        System.out.println("Sueldo: " + queEmpleado.calcula_sueldo(1200));
     }
     
 }
